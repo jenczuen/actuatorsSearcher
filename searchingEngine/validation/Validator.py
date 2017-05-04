6f96746a2e9a16d947fe99c450cc179534cc6797
Jedrzej Siemienski <jenczuen@gmail.com>
2017-05-04 19:12:18 +0200

    added calculating stroke from m to mm

--- searchingEngine/validation/Validator.py
+++ searchingEngine/validation/Validator.py
@@ -5,19 +5,22 @@ class Validator:
         self.actuator = actuator
         self.log_list = []
 
+    def passed_validation(self):
+        return len(self.log_list) == 0
+
     def validate_max_stroke(self, value):
-        if self.actuator.max_stroke_mm < value:
-            self.log_list.append("max_stroke_mm = %s, stroke = %s"
+        if self.actuator.max_stroke_mm < (value * 1000):
+            self.log_list.append("[ERROR MAX STROKE] max_stroke_mm = %s, stroke = %s"
                                  % (self.actuator.max_stroke_mm, value))
 
     def validate_Mz(self, value):
         if self.actuator.max_moment_Mz < value:
-            self.log_list.append("max_moment_Mz = %s, Mz = %s"
+            self.log_list.append("[ERROR MAX Mz] max_moment_Mz = %s, Mz = %s"
                                  % (self.actuator.max_moment_Mz, value))
 
     def validate_My(self, value):
         if self.actuator.max_moment_My < value:
-            self.log_list.append("max_moment_My = %s, My = %s"
+            self.log_list.append("[ERROR MAX My] max_moment_My = %s, My = %s"
                                  % (self.actuator.max_moment_My, value))
 
     def validate_Fa(self, Fa, Vmax):
@@ -30,25 +33,25 @@ class Validator:
 
     def _validate_Fa1(self, value):
         if self.actuator.max_effective_action_force_1 < value:
-            self.log_list.append("max_effective_action_force_1 = %s, Fa = %s"
+            self.log_list.append("[ERROR MAX Fa1] max_effective_action_force_1 = %s, Fa = %s"
                                  % (self.actuator.max_effective_action_force_1, value))
 
     def _validate_Fa2(self, value):
         if self.actuator.max_effective_action_force_2 < value:
-            self.log_list.append("max_effective_action_force_2 = %s, Fa = %s"
+            self.log_list.append("[ERROR MAX Fa2] max_effective_action_force_2 = %s, Fa = %s"
                                  % (self.actuator.max_effective_action_force_2, value))
 
     def _validate_Fa3(self, value):
         if self.actuator.max_effective_action_force_3 < value:
-            self.log_list.append("max_effective_action_force_3 = %s, Fa = %s"
+            self.log_list.append("[ERROR MAX Fa3] max_effective_action_force_3 = %s, Fa = %s"
                                  % (self.actuator.max_effective_action_force_3, value))
 
     def validate_Vmax(self, value):
         if self.actuator.max_speed < value:
-            self.log_list.append("max_speed = %s, Vmax = %s"
+            self.log_list.append("[ERROR MAX SPEED] max_speed = %s, Vmax = %s"
                                  % (self.actuator.max_speed, value))
 
     def validate_a(self, value):
         if self.actuator.max_acc < value:
-            self.log_list.append("max_acc = %s, a = %s"
+            self.log_list.append("[ERROR MAX ACC] max_acc = %s, a = %s"
                                  % (self.actuator.max_acc, value))
