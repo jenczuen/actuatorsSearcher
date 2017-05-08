562ca17c2c718f73fcefd2832867129c01d6f8a0
Jedrzej Siemienski <jenczuen@gmail.com>
2017-05-08 23:23:09 +0200

    added scaffold for mounting kid calculation

--- searchingEngine/code_generation/CodeGenerator.py
+++ searchingEngine/code_generation/CodeGenerator.py
@@ -2,6 +2,7 @@ import logging
 from searchingEngine.models import Actuator
 from searchingEngine.code_generation.CodeData import CodeData
 from searchingEngine.code_generation.Options import Options
+from searchingEngine.code_generation.ActuatorsToMotors import ActuatorsToMotors
 
 
 class CodeGenerator:
@@ -10,7 +11,7 @@ class CodeGenerator:
     def generate_codes_for_request(request):
         stroke = request['input_data[step]']
         result = {
-            "actuator_code_wrappers":[],
+            "actuator_code_wrappers": [],
             "drive_shaft_options": Options.drive_shaft_options,
             "magnetic_sensors_options": Options.magnetic_sensors_options,
             "profile_mounting_options": Options.profile_mounting_options
@@ -34,11 +35,36 @@ class CodeGenerator:
         code_data = CodeData()
         code_data.size = actuator_size
         code_data.stroke = stroke
+        code_data.mounting_kit = CodeGenerator.choose_mounting_kit(actuator.name)
 
         return {
             "name": actuator.name,
-            "code_data": code_data,
-            "mounting_kit_options": Options.mounting_kit_for_given_size[actuator_size]
+            "code_data": code_data
         }
+
+    @staticmethod
+    def choose_mounting_kit(actuator_name):
+        options = ActuatorsToMotors.map[actuator_name]
+        motors_options, gears_options = CodeGenerator.split_options(options)
+        motor = CodeGenerator.find_motor(motors_options)
+        if motor is not None:
+            return motor
+        motor, gear = CodeGenerator.find_motor_with_gear(gears_options)
+        if motor is not None and gear is not None:
+            return motor + ", " + gear
+
+        return "pucha2"
+
+    @staticmethod
+    def split_options(options):
+        return 0, 0
+
+    @staticmethod
+    def find_motor(motors_options):
+        return 0
+
+    @staticmethod
+    def find_motor_with_gear(gears_options):
+        return 0
 
 
