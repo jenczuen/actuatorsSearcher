69cce8d7e0b714aba5702f37aea6d5113487cf49
Jedrzej Siemienski <jenczuen@gmail.com>
2017-05-08 22:39:26 +0200

    added fixtures for motors matching

---
+++ searchingEngine/code_generation/CodeData.py
@@ -0,0 +1,16 @@
+class CodeData:
+    def __init__(self):
+        # sizes: 25, 32, 50
+        self.size = 25
+        self.type_of_actuator = 0
+        self.carriage = 0
+        self.drive_shaft = 0
+        self.gear_mounting = 0
+        self.mounting_kit = "0-"
+        self.stroke = 0
+        self.niro = 0
+        self.external_guide = 0
+        self.guide_position = 0
+        self.end_cap_mounting = 0
+        self.profile_mouting = "0"
+        self.magnetic_sensors = "0"
