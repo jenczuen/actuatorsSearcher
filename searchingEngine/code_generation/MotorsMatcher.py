940f23f9b53d8c9778a702e44065c5710b5e3d4f
Jedrzej Siemienski <jenczuen@gmail.com>
2017-05-09 21:43:38 +0200

    finished finding motor and gear

--- searchingEngine/code_generation/MotorsMatcher.py
+++ searchingEngine/code_generation/MotorsMatcher.py
@@ -48,19 +48,26 @@ class MotorsMatcher:
 
     @classmethod
     def __motor_speed_matches(cls, motor_max_rpm, speed, circumference):
-        speed_rpm = (60 * speed) / (math.pi * circumference)
-        return speed_rpm < motor_max_rpm
+        return cls.__rpm_min_for_m_s(speed, circumference) < motor_max_rpm
 
     @classmethod
-    def __find_motor_with_gear(cls, gears_options, torque, speed, circumference):
+    def __find_motor_with_gear(cls, gears_options, out_torque, out_speed, circumference):
         for gear_option in gears_options:
-            # sprawdzanie czy torque nie jest za duzy dla geara
+            cls.__check_max_torque_for_gear(gear_option, out_torque)
             motors_for_gears = GearsToMotors.map[gear_option]
             for ratio in [3, 5, 7, 10, 20]:
-                gear_torque = torque*ratio
-                # sprawdzanie czy gear_torque nie jest za duzy dla geara
-                gear_speed = speed/ratio
-                motor = cls.__find_motor(motors_for_gears, gear_torque, gear_speed, circumference)
+                in_torque = out_torque/ratio
+                cls.__check_max_torque_for_gear(gear_option, in_torque)
+                in_speed = out_speed*ratio
+                motor = cls.__find_motor(motors_for_gears, in_torque, in_speed, circumference)
                 if motor is not None:
                     return motor, gear_option
         return None
+
+    @classmethod
+    def __check_max_torque_for_gear(cls, gear, torque):
+        pass
+
+    @classmethod
+    def __rpm_min_for_m_s(cls, m_s, circumference):
+        return (60 * m_s) / (math.pi * circumference)
